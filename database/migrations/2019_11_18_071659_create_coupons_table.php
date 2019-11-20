95c241a2ed9461ddfbe8873205684a579b10a9cc
LebedevID <special_for_working@mail.ru>
2019-11-21 00:05:57 +0300

    Create shops and coupons tables migration

---
+++ database/migrations/2019_11_18_071659_create_coupons_table.php
@@ -0,0 +1,39 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateCouponsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('coupons', function (Blueprint $table) {
+            $table->bigIncrements('id');
+            $table->bigInteger('shop_id')->unsigned();
+            $table->foreign('shop_id')->references('id')->on('shops')->onDelete('cascade');
+            $table->string('title')->nullable();
+            $table->string('brand_name')->nullable();
+            $table->text('description')->nullable();
+            $table->string('exp_date')->nullable();
+            $table->text('image_url')->nullable();
+            $table->timestamp('created_at')->useCurrent();
+            $table->timestamp('updated_at')->useCurrent();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('coupons');
+    }
+}
