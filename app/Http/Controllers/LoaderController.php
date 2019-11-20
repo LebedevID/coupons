2dcefcb64f5c5aa07a357d581174a640a7542e02
LebedevID <special_for_working@mail.ru>
2019-11-21 00:26:10 +0300

    Create parse properties

--- app/Http/Controllers/LoaderController.php
+++ app/Http/Controllers/LoaderController.php
@@ -6,5 +6,12 @@ use Illuminate\Http\Request;
 
 class LoaderController extends Controller
 {
-    //
+    private $shops_source_url                   = 'https://www.coupons.com/store-loyalty-card-coupons/';
+    private $shop_link_class_name               = '.store-pod';
+    private $coupon_image_container_class_name  = '.media-object';
+    private $coupon_content_class_name          = '.media';
+    private $coupon_title_class_name            = '.pod_summary';
+    private $coupon_brand_class_name            = '.pod_brand';
+    private $coupon_description_class_name      = '.pod_description';
+    private $coupon_expiry_class_name           = '.pod_expiry';
 }
